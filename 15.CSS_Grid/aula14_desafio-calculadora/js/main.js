a3a862d30e2163d239ecec3d1433d0e878b7cccc
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-15 09:24:37 -0300

    identificando operadores e realizando os calculos

--- 15.CSS_Grid/aula14_desafio-calculadora/js/main.js
+++ 15.CSS_Grid/aula14_desafio-calculadora/js/main.js
@@ -1,22 +1,67 @@
 // Identifica os números e concatena
 let n1 = "0";
+let operacao = null;
+let n2 = '';
 
 function incluirDigito(digito) {
 
-    if(n1 === "0") {
-        n1 = digito;
+    if(operacao !== null) {
+        n2 += digito;
+        mostrarNoDisplay(n2);
     } else {
-        n1 += digito;
+        if(n1 === "0") {
+            n1 = digito;
+        } else {
+            n1 += digito;
+        }
+        mostrarNoDisplay(n1);
     }
-    console.log(n1);
+}
 
-    mostrarNoDisplay(n1);
 
+// Realizar os cálculos
+function calcular() {
+    let nCalculado = 0;
+    let _n1 = parseFloat(n1);
+    let _n2 = parseFloat(n2);
+
+    switch(operacao) {
+        case '+':
+            nCalculado = _n1 + _n2;
+            break;
+        case '-':
+            nCalculado = _n1 - _n2;
+            break;
+        case '*':
+            nCalculado = _n1 * _n2;
+            break;
+        case '/':
+            nCalculado = _n1 / _n2;
+            break; 
+    }
+    return nCalculado;
 }
 
 
 // Mostra no display
 function mostrarNoDisplay(valor) {
-    
+    document.querySelector('#display').innerHTML = valor;
 }
 
+
+// Identificar o símbolo da operação
+function iniciarCalculo(simbolo) {
+
+    if(operacao === null || n2 === '') {
+        operacao = simbolo;
+    } else {
+        let resultado = calcular();
+        n1 = resultado;
+        operacao = simbolo;
+        n2 = '';
+        mostrarNoDisplay(n1);
+    }
+
+
+    console.log(n1, operacao, n2);
+}
