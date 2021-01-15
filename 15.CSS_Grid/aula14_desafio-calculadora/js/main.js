1b1f2e416529eb701bd8097d0b9e00d6e4ad6532
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-15 03:02:39 -0300

    identificando numeros e concatenando

---
+++ 15.CSS_Grid/aula14_desafio-calculadora/js/main.js
@@ -0,0 +1,22 @@
+// Identifica os números e concatena
+let n1 = "0";
+
+function incluirDigito(digito) {
+
+    if(n1 === "0") {
+        n1 = digito;
+    } else {
+        n1 += digito;
+    }
+    console.log(n1);
+
+    mostrarNoDisplay(n1);
+
+}
+
+
+// Mostra no display
+function mostrarNoDisplay(valor) {
+    
+}
+
