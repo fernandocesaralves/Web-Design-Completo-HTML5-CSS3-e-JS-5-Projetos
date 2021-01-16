5c2ec07b03edfa9a40ee199b3aba133326f1ca28
Fernando César <fernandocesar.alves@hotmail.com>
2021-01-16 00:40:31 -0300

    finalizacao calculadora

--- 15.CSS_Grid/aula14_desafio-calculadora/js/main.js
+++ 15.CSS_Grid/aula14_desafio-calculadora/js/main.js
@@ -19,6 +19,28 @@ function incluirDigito(digito) {
 }
 
 
+let clicadoEmIgual = false;
+// Finaliza o cálculo com o sinal de =
+function finalizarCalculo() {
+    clicadoEmIgual = true;
+    let resultado = calcular();
+    n1 = resultado;
+    mostrarNoDisplay(n1);
+    console.log('n1', n1, 'operacao', operacao, 'n2', n2);
+}
+
+// Calcular porcentagem
+function obterPorcento() {
+    if(!n2) {
+        n2 = '';
+        mostrarNoDisplay(n1);
+    } else {
+        let porcento = n1 * n2 / 100;
+        n2 = porcento;
+        mostrarNoDisplay(n2);
+    }
+}
+
 // Realizar os cálculos
 function calcular() {
     let nCalculado = 0;
@@ -42,16 +64,33 @@ function calcular() {
     return nCalculado;
 }
 
+// Incluir ponto de número decimal em n2
+function incluirPonto() {
+    if(operacao && n2 === '') {
+        n2 = '0.';
+    } else if (operacao && n2) {
+        n2 += '.';
+    } else {
+        n1 += '.';
+    }
+}
 
-// Mostra no display
-function mostrarNoDisplay(valor) {
-    document.querySelector('#display').innerHTML = valor;
+
+// reinicia a calculadora
+function limpar() {
+    n1 = '0';
+    operacao = null;
+    n2 = '';
+    mostrarNoDisplay(n1);
 }
 
 
 // Identificar o símbolo da operação
 function iniciarCalculo(simbolo) {
-
+    if(clicadoEmIgual) {
+        clicadoEmIgual = false;
+        n2 = '';
+    }
     if(operacao === null || n2 === '') {
         operacao = simbolo;
     } else {
@@ -61,7 +100,10 @@ function iniciarCalculo(simbolo) {
         n2 = '';
         mostrarNoDisplay(n1);
     }
+}
 
 
-    console.log(n1, operacao, n2);
+// Mostra no display
+function mostrarNoDisplay(valor) {
+    document.querySelector('#display').innerHTML = valor;
 }
